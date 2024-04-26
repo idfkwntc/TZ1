c012875cf2e9fc80b13df42be4322992fac103cb
Магомед Идрисов <johh_wick_98@list.ru>
2024-04-26 23:49:14 +0300

    a

--- script.sh
+++ script.sh
@@ -2,6 +2,4 @@ echo "Файлы:"
 find $1 -type f -print
 echo "Папки:"
 find $1 -type d -print
-rm -rf $2
-mkdir -p $2
-find $1 -type f -exec bash -c "cp \"{}\" $2" \;
+find $1 -type f -exec cp {} $2 \;
